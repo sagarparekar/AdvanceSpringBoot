a242385cdf179b5d52232ef14031d7ac9df595bd
sagar parekar <sagarparekar11@gmail.com>
2023-01-04 22:53:48 +0530

    Removed git commands

--- CustomerManagment/src/main/java/com/cts/CustomerManagementApplication.java
+++ CustomerManagment/src/main/java/com/cts/CustomerManagementApplication.java
@@ -12,15 +12,3 @@ public class CustomerManagementApplication {
 
 }
 
-// GitHub Commands
-// 1. D:\Projects\gitclone>git clone https://github.com/sagarparekar/AdvanceSpringBoot.git
-// 2. D:\Projects\gitclone>cd AdvanceSpringBoot
-//
-// 3. D:\Projects\gitclone\AdvanceSpringBoot>git gui
-//
-// 4. D:\Projects\gitclone\AdvanceSpringBoot>git add .
-// 5. to configure user id - D:\Projects\gitclone\AdvanceSpringBoot>git config --global user.email "sagarparekar11@gmail.com"
-//D:\Projects\gitclone\AdvanceSpringBoot>git add .
-//
-//D:\Projects\gitclone\AdvanceSpringBoot>git commit -m "code updated"
-// D:\Projects\gitclone\AdvanceSpringBoot>git push

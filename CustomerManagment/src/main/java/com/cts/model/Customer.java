2fae3485affacfd9732239812701d06344e50377
sagar parekar <sagarparekar11@gmail.com>
2023-01-05 13:31:55 +0530

    validation dependency added

--- CustomerManagment/src/main/java/com/cts/model/Customer.java
+++ CustomerManagment/src/main/java/com/cts/model/Customer.java
@@ -5,28 +5,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "customer")
 public class Customer {
 
     @Id
     @GeneratedValue
+
     private long custId;
 
     private String custName;
 
+
     private String custAddress;
+
 
     private long custContactNumber;
 
+
     private double custAccountNumber;
+
 
     @JsonFormat(pattern = "dd-MM-yyyy", timezone = "Asia/Kolkata")
     private Date custDOB;
--- CustomerManagment/src/main/java/com/cts/repo/CustomerRepo.java
+++ CustomerManagment/src/main/java/com/cts/repo/CustomerRepo.java
@@ -4,8 +4,11 @@ import com.cts.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
+
 
 
 }

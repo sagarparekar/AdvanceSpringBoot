97d9a402b077f0b799b714575cffed2907477934
sagar parekar <sagarparekar11@gmail.com>
2023-01-16 22:50:09 +0530

    Validations added to pojo

--- Project_04_HRM/HRM/src/main/java/com/hrm/model/Employee.java
+++ Project_04_HRM/HRM/src/main/java/com/hrm/model/Employee.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -20,15 +22,19 @@ public class Employee {
     @GeneratedValue
     @Column(name = "emp_id")
     private int empId;
+
+    @Pattern(regexp = "[A-Za-z]*",message = "Employee First Name should not contain special character & Space")
     @Column(name = "emp_first_name")
     private String empFirstName;
 
+    @Pattern(regexp = "[A-Za-z]*",message = "Employee Last Name should not contain special character & Space")
     @Column(name = "emp_last_name")
     private String empLastName;
     @Column(name = "emp_address")
     private String empAddress;
     @Column(name = "emp_salary")
     private double empSalary;
+    @Pattern(regexp = "(\\+91|0)[0-9]{10}",message = "Please enter correct contact number")
     @Column(name = "emp_contact_number", unique = true)
     private String empContactNumber;
     @Column(name = "emp_dob")
@@ -37,6 +43,8 @@ public class Employee {
     @Column(name = "emp_email_id", unique = true)
     @Email(message = "Email must be valid like abc11@gmail.com")
     private String empEmailId;
+
+    @Size(min =4,message = "Password should be at least 4 character")
     @Column(name = "emp_password")
     private String empPassword;
 

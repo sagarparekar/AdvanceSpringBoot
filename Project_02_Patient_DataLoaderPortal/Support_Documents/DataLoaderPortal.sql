4eacc2ccf601f8292cc781fbf204568945a41460
sagar parekar <sagarparekar11@gmail.com>
2023-01-16 14:01:33 +0530

    HRM backend with validations and exception

--- Project_02_Patient_DataLoaderPortal/Support_Documents/DataLoaderPortal.sql
+++ Project_02_Patient_DataLoaderPortal/Support_Documents/DataLoaderPortal.sql
@@ -5,4 +5,4 @@ select * from USERS;
 select * from roles;
 select * from users_roles;
 -- INSERT INTO roles (id, name) VALUES (2, 'ROLE_USER');
--- insert into users_roles (user_id, role_id) values(2,2);
+-- insert into users_roles (user_id, role_id) values(3,1);
---
+++ Project_04_HRM/HRM/src/main/java/com/hrm/model/Employee.java
@@ -0,0 +1,4 @@
+package com.hrm.model;
+
+public class Employee {
+}

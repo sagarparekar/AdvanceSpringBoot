f7729cdf3ffd1afdd07d9848b4b46b4cbee4ebbc
sagar parekar <sagarparekar11@gmail.com>
2023-01-16 15:02:40 +0530

    getDataByFirstName custom method added in repo In Dao SignIn, SignUp, SaveAllData method added

--- Project_04_HRM/HRM/src/main/java/com/hrm/dao/EmployeeDaoImpl.java
+++ Project_04_HRM/HRM/src/main/java/com/hrm/dao/EmployeeDaoImpl.java
@@ -1,4 +1,42 @@
 package com.hrm.dao;
 
+import com.hrm.model.Employee;
+import com.hrm.repo.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
 public class EmployeeDaoImpl {
+
+    @Autowired
+    EmployeeRepository employeeRepositoryImpl;
+
+    public Employee signUp(Employee employee) {
+        return employeeRepositoryImpl.save(employee);
+    }
+
+    public boolean signIn(String empEmailId, String empPassword) {
+        boolean flag = false;
+
+        for (Employee employee : employeeRepositoryImpl.findAll()) {
+            if (employee.getEmpEmailId().equals(empEmailId) && employee.getEmpPassword().equals(empPassword)) {
+                flag = true;
+            }
+        }
+
+        return flag;
+    }
+
+    public List<Employee> saveAllData(List<Employee> employees) {
+        return employeeRepositoryImpl.saveAll(employees);
+    }
+
+    public Optional<Employee> getDataById(int empId) {
+        return employeeRepositoryImpl.findById(empId);
+    }
+
+
 }

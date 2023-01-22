6628e6f2619de24325ce3d2e3ab2bf1cf27fd8b2
sagar parekar <sagarparekar11@gmail.com>
2023-01-22 10:28:20 +0530

    Project managment application with spring secuirity

---
+++ Project_06_Project-managment-application/project-managment/src/main/java/com/learn/ProjectManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagmentApplication.class, args);
+	}
+
+}

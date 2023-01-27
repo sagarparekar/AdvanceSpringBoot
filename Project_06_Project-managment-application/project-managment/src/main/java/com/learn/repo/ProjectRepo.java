a98a57b52e04a107180521ea761464e001412a3b
sagar parekar <sagarparekar11@gmail.com>
2023-01-27 15:58:57 +0530

    Custom method find project by projectName

--- Project_06_Project-managment-application/project-managment/src/main/java/com/learn/repo/ProjectRepo.java
+++ Project_06_Project-managment-application/project-managment/src/main/java/com/learn/repo/ProjectRepo.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProjectRepo extends JpaRepository<Project, Long> {
 
+    public Project getProjectByprojectName(String projectName);
+
 }
